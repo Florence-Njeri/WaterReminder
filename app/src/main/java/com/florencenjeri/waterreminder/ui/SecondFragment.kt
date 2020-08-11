9427363a078436f31bc583e86b30e2e41c85152a
Florence-Njeri <flonsie17@gmail.com>
2020-08-11 19:04:34 +0300

    initial commit

--- app/src/main/java/com/florencenjeri/waterreminder/ui/SecondFragment.kt
+++ app/src/main/java/com/florencenjeri/waterreminder/ui/SecondFragment.kt
@@ -1,12 +1,10 @@
 package com.florencenjeri.waterreminder.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.florencenjeri.waterreminder.R
 
 /**
@@ -15,8 +13,8 @@ import com.florencenjeri.waterreminder.R
 class SecondFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
@@ -25,8 +23,5 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 }
