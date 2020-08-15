bbd24ce0068937f83012f10fa73f1c5028fa055f
Florence-Njeri <flonsie17@gmail.com>
2020-08-15 06:02:15 +0300

    set up all WorkManager dependencies in ApplicationModule.kt

--- app/src/main/java/com/florencenjeri/waterreminder/di/ApplicationModule.kt
+++ app/src/main/java/com/florencenjeri/waterreminder/di/ApplicationModule.kt
@@ -1,11 +1,13 @@
 package com.florencenjeri.waterreminder.di
 
 import android.content.Context
+import androidx.work.WorkManager
 import com.florencenjeri.waterreminder.prefs.UserPrefsManager
 import com.florencenjeri.waterreminder.ui.view.ProfileSettingsFragment
 import com.florencenjeri.waterreminder.utils.CredentialsValidator
 import com.florencenjeri.waterreminder.utils.SettingsCredentialsValidator
 import com.florencenjeri.waterreminder.utils.WorkManagerHelper
+import com.florencenjeri.waterreminder.workmanager.ReminderWorkManager
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
@@ -16,6 +18,8 @@ val applicationModule = module {
         scoped<CredentialsValidator> { SettingsCredentialsValidator() }
     }
     //WorkManager
+    single { WorkManager.getInstance(get()) }
     single { WorkManagerHelper(get()) }
+    single { ReminderWorkManager(androidContext(), get(), get()) }
 }
 private const val USER_PREFS = ""
