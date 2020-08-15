323bd124efc5499cbb61b1cfa97437c7a13bd974
Florence-Njeri <flonsie17@gmail.com>
2020-08-15 05:12:49 +0300

    add WorkManager and Notification to remind the user to drink water

---
+++ app/src/main/java/com/florencenjeri/waterreminder/workmanager/ReminderNotification.kt
@@ -0,0 +1,31 @@
+package com.florencenjeri.waterreminder.workmanager
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.florencenjeri.waterreminder.R
+import com.florencenjeri.waterreminder.ui.view.MainActivity
+
+private val NOTIFICATION_ID = 0
+fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
+
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+    val builder = NotificationCompat.Builder(
+        applicationContext,
+        applicationContext.getString(R.string.water_reminder_channel_id)
+    )
+        .setSmallIcon(R.drawable.ic_drink)
+        .setContentTitle(applicationContext.getString(R.string.notification_title))
+        .setContentText(messageBody)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
+    notify(NOTIFICATION_ID, builder.build())
+}
