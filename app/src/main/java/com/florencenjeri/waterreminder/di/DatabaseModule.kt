3233108c8c2a5b742517b73b958ec9de0009045f
Florence-Njeri <flonsie17@gmail.com>
2020-08-24 20:12:50 +0300

    initialize water database and dao in the database DI module

--- app/src/main/java/com/florencenjeri/waterreminder/di/DatabaseModule.kt
+++ app/src/main/java/com/florencenjeri/waterreminder/di/DatabaseModule.kt
@@ -1,10 +1,15 @@
 package com.florencenjeri.waterreminder.di
 
 import com.florencenjeri.waterreminder.database.UserSettingsDatabase
+import com.florencenjeri.waterreminder.database.WaterDataDatabase
 import org.koin.dsl.module
 
 val databaseModule = module {
     single { UserSettingsDatabase.getDatabase(get()) }
 
     single { get<UserSettingsDatabase>().userSettingsDao() }
+
+    single { WaterDataDatabase.getDatabase(get()) }
+
+    single { get<WaterDataDatabase>().waterDataDao() }
 }
